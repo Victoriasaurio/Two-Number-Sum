0c1b99953a7217234d784da24812fc23ed0feeb0
Victoriasaurio <victoriasaurio.guzmanc@gmail.com>
2021-03-13 11:06:04 -0600

    This exercise sum two values from an array to find a match

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 function twoNumberSum(array, targetSum) {
 	let sum = 0
-	for(let data of array){
-		sum += data
+	for(let data in array){
+		let suma = Number(data) + 1
+		for(let i=suma; i<array.length; i++){
+			sum = array[data] + array[i]
+			if(sum === targetSum) {
+				return console.log([array[data], array[i]])
+			}
+		}
 	}
-	
+	return console.log([])
   // Write your code here.
 }
+
+twoNumberSum([3,5,-4,8,11,1,-1,6], 2)
 
 // Do not edit the line below.
 exports.twoNumberSum = twoNumberSum;
